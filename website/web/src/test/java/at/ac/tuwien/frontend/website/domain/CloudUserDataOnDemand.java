787e6bd467fe5972f0a80eac089ead5719447556
Patonai <patonaipeter@gmail.com>
2012-10-23 19:15:57 +0200

    dummy website generated with spring roo TODOs: 1.) Login via credentials that are stored in the DB 2.) Connect User-Task in data model 3.) run on GAE 4.) configure other database instead HYPERION (?)

---
+++ website/web/src/test/java/at/ac/tuwien/frontend/website/domain/CloudUserDataOnDemand.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.frontend.website.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = CloudUser.class)
+public class CloudUserDataOnDemand {
+}
