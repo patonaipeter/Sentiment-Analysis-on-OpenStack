68450eb8353e4424ca4f55a7069336f0ee7d5e20
Andreas Rohner <andy641@gmx.at>
2012-11-05 17:36:35 +0100

    deletions

--- website/web/src/test/java/at/ac/tuwien/frontend/website/domain/CloudUserDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package at.ac.tuwien.frontend.website.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = CloudUser.class)
-public class CloudUserDataOnDemand {
-}
--- website/web/src/test/java/at/ac/tuwien/frontend/website/domain/TaskDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package at.ac.tuwien.frontend.website.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Task.class)
-public class TaskDataOnDemand {
-}
