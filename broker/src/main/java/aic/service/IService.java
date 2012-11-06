f721d87b3dc9c3e5c0f9ed89408ffcda2beea994
Andreas Rohner <andy641@gmx.at>
2012-11-06 12:34:34 +0100

    added dependencies for service in broker

--- broker/src/main/java/aic/service/IService.java
+++
@@ -1,9 +0,0 @@
-package aic.service;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IService extends Remote{
-	public double analyseSentiment(String company) throws RemoteException;
-
-}
