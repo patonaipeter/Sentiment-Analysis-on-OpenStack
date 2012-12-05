ee0f493a4159fdc7bfbfa82638de17cfbc86378d
Sebastian Geiger <sbastig@gmx.net>
2012-12-06 00:51:00 +0100

    Add a simple DataStore access example

--- sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/controller/SampleController.java
+++ sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/controller/SampleController.java
@@ -1,13 +1,25 @@
 package aic.appengine.sentimentanalysis.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
 
 @Controller
 public class SampleController extends AbstractController {
@@ -19,6 +31,41 @@ public class SampleController extends AbstractController {
 		System.out.println("SimpleController was called");
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("greeting", "Hello User");
+
+		String name = "peter";
+		String email = "email";
+
+		Key customerKey = KeyFactory.createKey("Customer", name);
+
+		Date date = new Date();
+		Entity customer = new Entity("Customer", customerKey);
+		customer.setProperty("name", name);
+		customer.setProperty("email", email);
+		customer.setProperty("date", date);
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		datastore.put(customer);
+
+		System.out.println(customer.getProperty("name"));
+
 		return mav;
+	}
+
+	// get all customers
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String listCustomer(ModelMap model) {
+
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		Query query = new Query("Customer").addSort("date",
+				Query.SortDirection.DESCENDING);
+		List<Entity> customers = datastore.prepare(query).asList(
+				FetchOptions.Builder.withLimit(10));
+
+		model.addAttribute("name", customers.get(0).getProperty("name"));
+
+		return "list";
+
 	}
 }
---
+++ sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/service/DataStoreAccess.java
@@ -0,0 +1,22 @@
+package aic.appengine.sentimentanalysis.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DataStoreAccess {
+	
+	/**
+	 * TODO:
+	 * 
+	 * 1. Initialize data store
+	 * 2. write a method that takes a name parameter and returns a sentiment value
+	 *
+	 */
+	
+	public double getSentiment(String name) {
+		// Call Map/Reduce algorithm here and do some really cool stuff...	
+		
+		return 0.0;
+	}
+}
