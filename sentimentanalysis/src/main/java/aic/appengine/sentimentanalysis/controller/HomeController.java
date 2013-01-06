e3015abe28c7f5f61c0e01153cb1d7c457712c31
Andreas Rohner <andy641@gmx.at>
2013-01-06 13:52:20 +0100

    added tweet upload support (NOT TESTED and with NO INTERFACE), insert initial set of tweets from war file

--- sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/controller/HomeController.java
+++ sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/controller/HomeController.java
@@ -1,19 +1,35 @@
 package aic.appengine.sentimentanalysis.controller;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
 @Controller
-public class HomeController {
+public class HomeController{
 
 	/**
 	 * This is the service bean that exposes the sentiment lookup functionality.
@@ -29,6 +45,111 @@ public class HomeController {
 	 */
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView showIndexPage(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping(value="/uploaddata", method=RequestMethod.POST)
+	public ModelAndView uploadData(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		//TODO not tested
+		//upload should be done in java with raw POST request
+        InputStream is = request.getInputStream();
+        
+        
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				new GZIPInputStream(is)));
+
+        String line=null;
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        JSONParser p = new JSONParser();
+
+        int count=0;
+        while ((line = in.readLine()) != null){
+        	
+        	try {
+				JSONObject obj=(JSONObject)p.parse(line);
+				//insert into datastore
+				if (obj.get("name") != null && obj.get("text") != null
+						&& obj.get("sentiment") != null
+						&& obj.get("keywords") != null) {
+					
+					Entity tweet = new Entity("tweets");
+					tweet.setUnindexedProperty("name", obj.get("name"));
+					tweet.setUnindexedProperty("text", obj.get("text"));
+					tweet.setUnindexedProperty("sentiment", obj.get("sentiment"));
+					List<String> arr = (List<String>)obj.get("keywords");
+					tweet.setProperty("keywords",arr);
+			        datastore.put(tweet);
+				}
+
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+        	System.out.print("Inserted: " + count + "\r");
+        	count++;
+        }
+        in.close();
+        
+		return new ModelAndView("index");
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/insertdata", method=RequestMethod.GET)
+	public ModelAndView insertData(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        /*
+		//TODO: Problem
+        //does not work because GAE does not allow streaming. The call to
+        //getInputStream downloads the entire file before returning
+        //additionally there is a 1MB size limit.
+		
+		//180mb contains 2.6 million tweets
+		//URL tweets = new URL("http://web.student.tuwien.ac.at/~e0502196/tweets.json.gz");
+		//contains only 12000 tweets
+		URL tweets = new URL("http://web.student.tuwien.ac.at/~e0502196/smalltweets.json.gz");
+        URLConnection con = tweets.openConnection();
+        //default timeout is 5 seconds
+        con.setConnectTimeout(60000);
+        con.setReadTimeout(60000);
+
+        //InputStream is=con.getInputStream();*/
+        
+        //this works but blows up the war file to 200mb
+        ServletContext context = request.getSession().getServletContext();
+        InputStream is = context.getResourceAsStream("/WEB-INF/tweets.json.gz");
+        
+        
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				new GZIPInputStream(is)));
+
+        String line=null;
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        JSONParser p = new JSONParser();
+
+        int count=0;
+        while ((line = in.readLine()) != null){
+        	
+        	try {
+				JSONObject obj=(JSONObject)p.parse(line);
+				//insert into datastore
+				if (obj.get("name") != null && obj.get("text") != null
+						&& obj.get("sentiment") != null
+						&& obj.get("keywords") != null) {
+					
+					Entity tweet = new Entity("tweets");
+					tweet.setUnindexedProperty("name", obj.get("name"));
+					tweet.setUnindexedProperty("text", obj.get("text"));
+					tweet.setUnindexedProperty("sentiment", obj.get("sentiment"));
+					List<String> arr = (List<String>)obj.get("keywords");
+					tweet.setProperty("keywords",arr);
+			        datastore.put(tweet);
+				}
+
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+        	System.out.print("Inserted: " + count + "\r");
+        	count++;
+        }
+        in.close();
 		return new ModelAndView("index");
 	}
 
