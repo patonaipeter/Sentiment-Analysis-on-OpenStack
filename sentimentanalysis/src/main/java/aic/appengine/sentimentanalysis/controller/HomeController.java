f44a5ac712fcf3eb84da82d0b40e4108042aab99
Sebastian Geiger <sbastig@gmx.net>
2013-01-13 22:42:15 +0100

    Update Home controller and security constraints

--- sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/controller/HomeController.java
+++ sentimentanalysis/src/main/java/aic/appengine/sentimentanalysis/controller/HomeController.java
@@ -33,39 +33,10 @@ public class HomeController{
 	
 	/**
 	 * This serves the index page, that shows the basic information about our sentiment analysis and
-	 * displays a form to enter the taskname and query.
+	 * displays a form to enter the task name and query.
 	 */
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView showIndexPage(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		return new ModelAndView("index");
-	}
-	
-	@RequestMapping(value="/tasks/insertdata", method=RequestMethod.GET)
-	public ModelAndView insertData(HttpServletRequest request, HttpServletResponse response) throws IOException{
-
-        //this works but blows up the war file to 200mb
-		ServletContext context = request.getSession().getServletContext();
-        InputStream is = context.getResourceAsStream("/WEB-INF/tweets.json.gz");
-        
-        DataStoreAccess.initDatastore(is);
-        
-		return new ModelAndView("index");
-	}
-	
-	/*
-	 * use this url to init data!
-	 * it starts a background task that can run for up
-	 * to 10 minutes
-	 * 
-	 * if you start /insertdata directly it will be killed after 60 seconds
-	 */
-	@RequestMapping(value="/init", method=RequestMethod.GET)
-	public ModelAndView initData(HttpServletRequest request, HttpServletResponse response) throws IOException{
-
-        Queue queue = QueueFactory.getDefaultQueue();
-        //starts background task with url /insertdata
-        queue.add(withUrl("/tasks/insertdata").method(Method.GET));
-        
 		return new ModelAndView("index");
 	}
 
@@ -88,45 +59,24 @@ public class HomeController{
 		//TODO not tested
 		//upload should be done in java with raw POST request
 		System.out.println("Deleting Data");
-        DataStoreAccess.deleteTweets();
-        
+		DataStoreAccess.deleteTweets();
+
 		return new ModelAndView("index");
 	}
 	
 	
+	/**
+	 * This method is used by the DataUploader (Upload.java) to upload the tweets to the data store
+	 */
 	@RequestMapping(value="/uploaddata", method=RequestMethod.POST)
 	public ModelAndView uploadData(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		//TODO not tested
 		//upload should be done in java with raw POST request
 		System.out.println("Uploading Data");
-        InputStream is = request.getInputStream();
-        
-        DataStoreAccess.initDatastore(is);
-        
+		InputStream is = request.getInputStream();
+
+		DataStoreAccess.initDatastore(is);
+
 		return new ModelAndView("index");
 	}
-	
-	/*@RequestMapping(value="/downloaddata", method=RequestMethod.GET)
-	public ModelAndView downloadData(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		//TODO: Problem
-        //does not work because GAE does not allow streaming. The call to
-        //getInputStream downloads the entire file before returning
-        //additionally there is a 1MB size limit.
-		
-		//180mb contains 2.6 million tweets
-		//URL tweets = new URL("http://web.student.tuwien.ac.at/~e0502196/tweets.json.gz");
-		//contains only 12000 tweets
-		URL tweets = new URL("http://web.student.tuwien.ac.at/~e0502196/smalltweets.json.gz");
-        URLConnection con = tweets.openConnection();
-        //default timeout is 5 seconds
-        con.setConnectTimeout(60000);
-        con.setReadTimeout(60000);
-
-        InputStream is=con.getInputStream();
-        
-        DataStoreAccess.initDatastore(is);
-        
-		return new ModelAndView("index");
-	}*/
-
 }
